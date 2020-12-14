b33c3ffe2cab891d7e48ff15b16029a74cf98b83
main <49618395+ZukiGA@users.noreply.github.com>
2020-12-14 17:33:57 -0600

    ProgressCircle's animation added. Files saved in folders

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, Text} from 'react-native'
-import ProgressChart from './ProgressCircle'
-import {styles} from './Styles'
+import ProgressChart from '../components/ProgressCircle'
+import {styles} from '../styles/styles'
 
 
 
@@ -9,7 +9,7 @@ export default class HomePageComponent extends React.Component{
   render(){
     return (
       <View>
-        <Text style = {styles.HomePageTitle}> HomePage </Text>
+        <Text style = {styles.HomePageTitle}> Bienvenido </Text>
         <ProgressChart/>
       </View>
     );
